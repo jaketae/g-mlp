cd5d92f022b514fd031e9ba37e87c3a74edb1c69
Jake Tae <jaesungtae@gmail.com>
2021-05-19 03:27:31 +0900

    feature: implement gmlp backbone

---
+++ g_mlp/core.py
@@ -0,0 +1,46 @@
+from torch import nn
+from torch.nn import functional as F
+
+
+class SpatialGatingUnit(nn.Module):
+    def __init__(self, d_ffn, seq_len):
+        super().__init__()
+        self.norm = nn.LayerNorm(d_ffn)
+        self.spatial_proj = nn.Conv1d(seq_len, seq_len, kernel_size=1)
+        nn.init.constant_(self.spatial_proj.bias, 1.0)
+
+    def forward(self, x):
+        u, v = x.chunk(2, dim=-1)
+        v = self.norm(v)
+        v = self.spatial_proj(v)
+        out = u * v
+        return out
+
+
+class GMLPBlock(nn.Module):
+    def __init__(self, d_model, d_ffn, seq_len):
+        super().__init__()
+        self.norm = nn.LayerNorm(d_model)
+        self.channel_proj1 = nn.Linear(d_model, d_ffn * 2)
+        self.channel_proj2 = nn.Linear(d_ffn, d_model)
+        self.sgu = SpatialGatingUnit(d_ffn, seq_len)
+
+    def forward(self, x):
+        residual = x
+        x = self.norm(x)
+        x = F.gelu(self.channel_proj1(x))
+        x = self.sgu(x)
+        x = self.channel_proj2(x)
+        out = x + residual
+        return out
+
+
+class GMLP(nn.Module):
+    def __init__(self, d_model, d_ffn, seq_len, num_layers):
+        super().__init__()
+        self.model = nn.Sequential(
+            *[GMLPBlock(d_model, d_ffn, seq_len) for _ in range(num_layers)]
+        )
+
+    def forward(self, x):
+        return self.model(x)
