5c612506a047ea04e08913aa7668955d6da2d2cc
Jake Tae <jaesungtae@gmail.com>
2021-05-19 03:42:33 +0900

    feature: add models for nlp & cv

--- g_mlp/core.py
+++ g_mlp/core.py
@@ -17,7 +17,7 @@ class SpatialGatingUnit(nn.Module):
         return out
 
 
-class GMLPBlock(nn.Module):
+class gMLPBlock(nn.Module):
     def __init__(self, d_model, d_ffn, seq_len):
         super().__init__()
         self.norm = nn.LayerNorm(d_model)
@@ -35,12 +35,63 @@ class GMLPBlock(nn.Module):
         return out
 
 
-class GMLP(nn.Module):
-    def __init__(self, d_model, d_ffn, seq_len, num_layers):
+class gMLP(nn.Module):
+    def __init__(self, d_model=256, d_ffn=512, seq_len=256, num_layers=6):
         super().__init__()
         self.model = nn.Sequential(
-            *[GMLPBlock(d_model, d_ffn, seq_len) for _ in range(num_layers)]
+            *[gMLPBlock(d_model, d_ffn, seq_len) for _ in range(num_layers)]
         )
 
     def forward(self, x):
         return self.model(x)
+
+
+class gMLPForLanguageModeling(gMLP):
+    def __init__(
+        self, num_tokens=10000, d_model=256, d_ffn=512, seq_len=256, num_layers=6
+    ):
+        super().__init__(d_model, d_ffn, seq_len, num_layers)
+        self.embed = nn.Embedding(num_tokens, d_model)
+
+    def forward(self, x):
+        embedding = self.embed(x)
+        out = self.model(embedding)
+        return out
+
+
+def check_sizes(image_size, patch_size):
+    sqrt_num_patches, remainder = divmod(image_size, patch_size)
+    assert remainder == 0, "`image_size` must be divisibe by `patch_size`"
+    num_patches = sqrt_num_patches ** 2
+    return num_patches
+
+
+class gMLPForImageClassification(gMLP):
+    def __init__(
+        self,
+        image_size=256,
+        patch_size=16,
+        in_channels=3,
+        num_classes=1000,
+        d_model=256,
+        d_ffn=512,
+        seq_len=256,
+        num_layers=6,
+    ):
+        num_patches = check_sizes(image_size, patch_size)
+        super().__init__(d_model, d_ffn, seq_len, num_layers)
+        self.patcher = nn.Conv2d(
+            in_channels, d_model, kernel_size=patch_size, stride=patch_size
+        )
+        self.classifier = nn.Linear(d_model, num_classes)
+
+    def forward(self, x):
+        patches = self.patcher(x)
+        batch_size, num_channels, _, _ = patches.shape
+        patches = patches.permute(0, 2, 3, 1)
+        patches = patches.view(batch_size, -1, num_channels)
+        embedding = self.model(patches)
+        embedding = embedding.mean(dim=1)
+        out = self.classifier(embedding)
+        return out
+
